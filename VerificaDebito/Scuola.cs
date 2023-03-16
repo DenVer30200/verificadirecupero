a442aec605f0a8aff8ad97991bd176634a95adbb
Deniz Verstivschii <DENIZ.VERSTIVSCHII@local.ittcesena.it>
2023-03-16 09:24:32 +0100

    prima versione della classe scuola

---
+++ VerificaDebito/Scuola.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerificaDebito
+{
+    internal class Scuola
+    {
+        public Scuola(string nome,string indirizzo, string orariApertura, string orariChiusura)
+        {
+
+        }
+
+        internal Alunno alunno = new();
+
+       
+        
+        public List <Alunno> datiMemorizzati { get; set; }
+        
+        public void aggiuntaStudente()
+        {
+
+        }
+
+        public void ricercaDiUnAlunno()
+        {
+
+        }
+
+
+    }
+}
